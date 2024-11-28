cdda814dac67760bd104f0c7c6c9ab06e99addf6
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2024-11-28 18:55:09 +0000

    Lesson 22: Copy from lesson 20

---
+++ lesson22/books-server-config.mjs
@@ -0,0 +1,45 @@
+/**
+ * Configures the express HTTP application (including routes and middlewares)
+ */
+
+import express from 'express'
+import * as api from './books-api.mjs' 
+
+console.log("Server-config loaded")
+
+export default function(app) {
+    app.use(express.json())
+    app.use(countReq, showRequestData)
+    app.use(api.extractToken)
+
+    // Web Application Resources URIs
+    const RESOURCES = {
+        // Resource URI that represents ALL Books
+        BOOKS: '/api/books',    
+        // Resource URI that represents ONE Book
+        BOOK: '/api/books/:bookId'
+    }
+
+    // Web Application Routes
+    
+    app.get(RESOURCES.BOOKS, api.getBooks)
+    app.post(RESOURCES.BOOKS, api.addBook)
+
+    app.get(RESOURCES.BOOK, api.getBook)
+    app.put(RESOURCES.BOOK, api.updateBook)
+    app.delete(RESOURCES.BOOK, api.deleteBook)
+
+    let count = 1
+    function countReq(req, rsp, next) {
+        console.log(`Number of requests: ${count++}`)
+        next()
+    }
+
+    function showRequestData(req, rsp, next) {
+        console.log(`Request method: ${req.method}`)
+        console.log(`Request uri: ${req.originalUrl}`)
+        next()
+    }
+
+}
+
