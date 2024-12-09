12c0179259b80ca77bc4cd78af5273ae15c3fda8
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2024-12-09 20:35:21 +0000

    Lesson 24: Initial web site development - Non Functional

--- lesson24/books-server-config.mjs
+++ lesson24/books-server-config.mjs
@@ -9,11 +9,13 @@ import booksDataInit from './data/books-data-es.mjs'
 import * as usersData from './data/users-data-mem.mjs'
 import booksServiceInit from './books-service.mjs'
 import apiInit from './books-api.mjs' 
+import siteInit from './books-web-site.mjs' 
 
 const booksData = booksDataInit()
 
 const booksService = booksServiceInit(booksData, usersData)
 const api = apiInit(booksService)
+const site = siteInit()
 
 console.log("Server-config loaded")
 
@@ -23,21 +25,38 @@ export default function(app) {
     app.use(api.extractToken)
 
     // Web Application Resources URIs
-    const RESOURCES = {
+    const RESOURCES_API = {
         // Resource URI that represents ALL Books
         BOOKS: '/api/books',    
         // Resource URI that represents ONE Book
         BOOK: '/api/books/:bookId'
     }
 
-    // Web Application Routes
-    
-    app.get(RESOURCES.BOOKS, api.getBooks)
-    app.post(RESOURCES.BOOKS, api.addBook)
+    const RESOURCES_WEB_SITE = {
+        // Resource URI that represents ALL Books
+        BOOKS: '/books',    
+        // Resource URI that represents ONE Book
+        BOOK: '/books/:bookId',
+        BOOK_EDIT: `${BOOK}`/edit
+    }
 
-    app.get(RESOURCES.BOOK, api.getBook)
-    app.put(RESOURCES.BOOK, api.updateBook)
-    app.delete(RESOURCES.BOOK, api.deleteBook)
+    // Web Api Application Routes
+    app.get(RESOURCES_API.BOOKS, api.getBooks)
+    app.post(RESOURCES_API.BOOKS, api.addBook)
+
+    app.get(RESOURCES_API.BOOK, api.getBook)
+    app.put(RESOURCES_API.BOOK, api.updateBook)
+    app.delete(RESOURCES_API.BOOK, api.deleteBook)
+
+    // Web Site Application Routes
+    app.get(RESOURCES_WEB_SITE.BOOKS, site.getBooks)
+    app.post(RESOURCES_API.BOOKS, site.addBook)
+
+    app.get(RESOURCES_WEB_SITE.BOOKS, site.getBook)
+
+    app.get(RESOURCES_WEB_SITE.BOOK, api.updateBook)
+    app.put(RESOURCES_WEB_SITE.BOOK, api.updateBook)
+
 
     let count = 1
     function countReq(req, rsp, next) {
